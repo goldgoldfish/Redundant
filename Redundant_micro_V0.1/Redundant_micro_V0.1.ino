2c4bfc44826bd13c715f2e65bf9cdde1784fc54a
Unknown <ben.wedemire@gmail.com>
2017-06-29 20:04:49 -0400

    Testing Branch
    This will help with testing

--- Redundant_micro_V0.1/Redundant_micro_V0.1.ino
+++ Redundant_micro_V0.1/Redundant_micro_V0.1.ino
@@ -18,60 +18,70 @@ void setup() { //begin setup
   pinMode(masterLED, OUTPUT); //set master LED pin to output
   pinMode(redundant, OUTPUT); //set redundant LED pin to output
   pinMode(LED, OUTPUT); //set LED pin to output
-
+  //start serial
   Serial.begin(9600); //start up serial communication on pins 0 and 1.
+  //do startup
   startup();
 } //end setup
 
 void loop() { //begin loop
-  curr_time_micro = micros(); //get the current time
   int inByte = 0; ////create temp for serial in
-    while (Serial.available() > 0) {
-    inByte = Serial.read(); //read the data on the serial port
-    if (inByte == 170) {
-      is_Master = 0; //set this micro to slave mode
-      last_heartbeat_rec = micros(); //get the current time
-      digitalWrite(masterLED, is_Master); //update the LED
+  curr_time_micro = micros(); //get the current time
+  prev_time_micro = curr_time_micro;
+  while(1) {
+    curr_time_micro = micros(); //get the current time
+    while (Serial.available()) {
+      inByte = Serial.read(); //read the data on the serial port
+      if (inByte == 89) {
+        is_Master = 0; //set this micro to slave mode
+        last_heartbeat_rec = micros(); //get the current time
+        digitalWrite(masterLED, is_Master); //update the LED
+      } //end if
+    } //end while
+    if ((is_Master) && (curr_time_micro >= (prev_time_micro + 1000))) { //if the micro is the master and 1ms has elapsed
+      digitalWrite(LED, HIGH);   // turn the LED on
+      heartbeat(); //send heartbeat
+      prev_time_micro = curr_time_micro;
+      Serial.write("if\n");
     } //end if
-  } //end if
-  if ((is_Master) && (curr_time_micro >= prev_time_micro + 1000)) { //if the micro is the master and 1ms has elapsed
-    digitalWrite(LED, HIGH);   // turn the LED on
-    heartbeat(); //perform heartbeat
-    prev_time_micro = curr_time_micro;
-  } //end if
-  else if (!(is_Master) && (last_heartbeat_rec + 100000 < curr_time_micro)) { //change mirco to master
-    is_Master = 1; //set this micro to master mode
+    else if (!(is_Master) && (last_heartbeat_rec + 20000 < curr_time_micro)) { //change mirco to master
+      is_Master = 1; //set this micro to master mode
+      digitalWrite(LED, HIGH); // turn the LED on
+      heartbeat(); //send heartbeat
+      prev_time_micro = curr_time_micro;
+      Serial.write("else if\n");
+    } //end else if
+    else { //do stuff for slave mode
+      digitalWrite(LED, LOW); //turn LED off
+      Serial.write("I am slave\n");
+    } //end else
     digitalWrite(masterLED, is_Master); //update LED
-    heartbeat(); //send heartbeat
-    prev_time_micro = curr_time_micro;
-  } //end else if
-  else { //do calculations here
-    
-  } //end else
+    Serial.write("iter\n");
+  } //end while
 } //end loop
 
 void startup() { //begin startup
   unsigned long time_begin = millis(); //time that startup started
-  unsigned long time_end = millis(); //current time in millis
-  int no_cmmd = 1; //flag for if the cmmd has been received
+  unsigned long time_curr = millis(); //current time in millis
+  int no_cmmd = 1; //flag for if a cmmd has not been received
   int inByte = 0; //create temp for serial in
-  while ((time_end < (time_begin + 2000)) && (no_cmmd)) { //check if 2 secs have passed
-    while (Serial.available() > 0) { //check if there is something on the serial port to read
+  while ((time_curr < (time_begin + 2000)) && (no_cmmd)) { //check if 2 secs have passed
+    while (Serial.available()) { //check if there is something on the serial port to read
       inByte = Serial.read(); //read the data on the serial port
-      if (inByte == 170) {
+      if (inByte == 89) {
         is_Master = 0; //set the master status to be false
-        last_heartbeat_rec = micros(); //get the current time
-        no_cmmd = 0;
+        last_heartbeat_rec = micros(); //get the current time in uS
+        no_cmmd = 0; //a command has been receiced
       } //end if
     } //end while
-    time_end = millis(); //get current time.
+    time_curr = millis(); //get current time.
   } //end while
   //if another controller has not assumed command this one will
   digitalWrite(masterLED, is_Master); //update status LED
-  if (is_Master) heartbeat(); //if controller is still master heartbeat
+  Serial.write("Done Startup\n");
 } //end startup
 
 void heartbeat() {
-  Serial.write(170); //send a value of 170 to indicate this controller is the master
+  Serial.write("I am master\n"); //send a value of 170 to indicate this controller is the master
   Serial.flush(); //wait for the transfer to be complete
 } //end heartbeat
